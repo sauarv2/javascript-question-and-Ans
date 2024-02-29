a9695c350b2f8f074dc7b0d51739902f184e43d4
saurav saha <sauravsaha024@gmail.com>
2024-02-29 17:19:16 +0530

    p

---
+++ drink-sorting/scripr.js
@@ -0,0 +1,32 @@
+
+    // QUESTION*****************************************************************************
+
+
+    /*You will be given an array of drinks, with each drink being an object with two properties: name and price. Create a function that has the drinks array as an argument and return the drinks objects sorted by price in ascending order.
+
+Assume that the following array of drink objects needs to be sorted:
+
+drinks = [
+  {name: "lemonade", price: 50},
+  {name: "lime", price: 10}
+]
+The output of the sorted drinks object will be:
+
+Examples
+sortDrinkByPrice(drinks) ➞ [{name: "lime", price: 10}, {name: "lemonade", price: 50}]
+*/
+function sortDrinkByPrice(drink){
+
+return drink.sort((drink[0].price ,drink[1].price) => {
+
+})
+
+  return drink[1].price
+
+}
+
+
+console.log(sortDrinkByPrice(drinks = [
+      {name: "lemonade", price: 50},
+       {name: "lime", price: 10}
+     ]));
---
+++ olderthan-me/script.js
@@ -0,0 +1,40 @@
+// QUESTION*********************************************************
+/*
+Create a method in the Person class which returns how another person's age compares. Given the instances p1, p2 and p3, which will be initialised with the attributes name and age, return a sentence in the following format:
+
+{other person name} is {older than / younger than / the same age as} me.
+
+p1 = Person("Samuel", 24)
+p2 = Person("Joel", 36)
+p3 = Person("Lily", 24)
+p1.compareAge(p2) ➞ "Joel is older than me."
+
+p2.compareAge(p1) ➞ "Samuel is younger than me."
+
+p1.compareAge(p3) ➞ "Lily is the same age as me."
+*/
+
+  class Person{
+    constructor(name,age){
+        this.name = name;
+        this.age = age;
+    }
+
+    compareAge(otherPerson){
+        if(this.age<otherPerson){
+            return `${otherPerson.name} is older than me.`;
+        } else if (this.age > otherPerson.age) {
+            return `${otherPerson.name} is younger than me.`;
+          } else {
+            return `${otherPerson.name} is the same age as me.`;
+          }
+    }
+  }
+
+const p1 = new Person("Samuel", 24);
+const p2 = new Person("Joel", 36);
+const p3 = new Person("Lily", 24);
+
+console.log(p1.compareAge(p2)); // Output: "Joel is older than me."
+console.log(p2.compareAge(p1)); // Output: "Samuel is younger than me."
+console.log(p1.compareAge(p3)); // Output: "Lily is the same age as me."
