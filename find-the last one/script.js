f9c29ea1a94fa0a39d5300ccd4bd7e84c5023644
saurav saha <sauravsaha024@gmail.com>
2024-07-30 10:56:06 +0530

    comment

--- find-the last one/script.js
+++ find-the last one/script.js
@@ -1,3 +1,5 @@
+// This question tests the understanding of the findLast method in JavaScript, which returns the value of the last element in the array that satisfies the provided testing function.
+
 let arry = [4, 1, 3, 1, 2, 6, 5, 7, 2, 3, 4, 9];
 
 let lastOne = arry.findLast((i) => i > 5);
