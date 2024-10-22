10e94183e11d34528f4b7c50c69b75f5eddebba9
saurav saha <sauravsaha024@gmail.com>
2024-10-22 18:21:25 +0530

    adding different method

--- calulate-difference/script.js
+++ calulate-difference/script.js
@@ -8,7 +8,8 @@ calculateDifference({ skate: 10, painting: 20 }, 19) ➞ 11
 
 calculateDifference({ skate: 200, painting: 200, shoes: 1 }, 400) ➞ 1
 */
-
+// 1st method*****************************
+/*
 function calculateDifference(a, d) {
   let valus = Object.values(a);
   //covert values into arrey
@@ -19,6 +20,37 @@ function calculateDifference(a, d) {
   });
 
   return op - d;
+}
+  */
+
+// 2nd method using reduce*****************
+/*
+function calculateDifference(a, d) {
+  let valus = Object.values(a);
+  //covert values into arrey
+  // let op = 0;
+
+  const avg = valus.reduce((acc, curr) => acc + curr, 0 - d);
+  // valus.forEach((vl) => {
+  //   return (op += vl);
+  // });
+
+  return avg;
+}
+
+
+*/
+
+// 3Rd method using reduce******************************-------------
+
+function calculateDifference(a, d) {
+  let valus = Object.values(a);
+  let int = 0;
+
+  for (let i = 0; i < valus.length; i++) {
+    int += valus[i];
+  }
+  return int - d;
 }
 
 console.log(calculateDifference({ "baseball bat": 20 }, 5));
