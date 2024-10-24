33b07a9c12326b7e8fae456956001be2a028eeb2
saurav saha <sauravsaha024@gmail.com>
2024-10-24 15:00:42 +0530

    adding another method

--- Count Instances of a Character in a String/script.js
+++ Count Instances of a Character in a String/script.js
@@ -1,6 +1,8 @@
 // Count Instances of a Character in a String
 // Create a function that takes two strings as arguments and returns the number of times the first string (the single character) is found in the second string
 
+// 1st  method**************************
+/*
 function charCount(a, b) {
   let count = 0;
 
@@ -11,6 +13,27 @@ function charCount(a, b) {
   }
   return count;
 }
+  */
+
+// 2nd method   ***********************************************************************
+
+// using map method
+function charCount(a, str) {
+  let count = 0;
+
+  // Use String split() method to
+  // Convert String to an Array
+  let arr = str.split("");
+
+  arr.map((ar) => {
+    if (ar === a) {
+      count++;
+    }
+  });
+
+  return count;
+}
+
 console.log(charCount("a", "edabit"));
 // ➞ 1
 console.log(charCount("c", "Chamber of secrets"));
