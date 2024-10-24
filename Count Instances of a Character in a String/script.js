bd5e3ae752e6a119357e50fdcd1e3915d5d4630b
saurav saha <sauravsaha024@gmail.com>
2024-10-24 15:45:47 +0530

    done

--- Count Instances of a Character in a String/script.js
+++ Count Instances of a Character in a String/script.js
@@ -2,6 +2,7 @@
 // Create a function that takes two strings as arguments and returns the number of times the first string (the single character) is found in the second string
 
 // 1st  method**************************
+// for loop
 /*
 function charCount(a, b) {
   let count = 0;
@@ -18,12 +19,59 @@ function charCount(a, b) {
 // 2nd method   ***********************************************************************
 
 // using map method
+/*
 function charCount(a, str) {
   let count = 0;
 
   // Use String split() method to
   // Convert String to an Array
   let arr = str.split("");
+
+  arr.map((ar) => {
+    if (ar === a) {
+      count++;
+    }
+  });
+
+  return count;
+}
+*/
+
+// 3rd mthod******************************************
+// for of loop
+/*
+function charCount(a, str) {
+  let count = 0;
+
+  // Use String split() method to
+  // Convert String to an Array
+  let arr = [...str];
+
+  for (ar of arr) {
+    if (ar === a) {
+      count++;
+    }
+  }
+
+  return count;
+}
+*/
+
+// 4th method**************************************************
+//using push method convert str to arr----------------------
+
+function charCount(a, str) {
+  let count = 0;
+
+  let arr = [];
+
+  let arrstr;
+
+  for (let i = 0; i < str.length; i++) {
+    arrstr = str[i];
+
+    arr.push(arrstr);
+  }
 
   arr.map((ar) => {
     if (ar === a) {
