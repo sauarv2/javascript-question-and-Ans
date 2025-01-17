a9cb48cff5fce5e613f65f4ad114ed50d391dc82
saurav saha <sauravsaha024@gmail.com>
2025-01-17 18:29:24 +0530

    new folder created

---
+++ js-interview1/no.1/script.js
@@ -0,0 +1,38 @@
+// No.1 ------------------
+/*
+// modfy the frist index value***************
+
+let name=  "js";
+
+ name[1] = "q"
+ 
+//  name=> js
+//  its not modified because string is because string is immutable 
+
+let name=  "js";
+
+ name = "q"
+
+//  name => q
+
+//  its not modified the string . it is modify the variable
+
+---------------------**************-----------------------
+*/
+
+/*
+// No.2***************************
+
+let num = 1;
+const sum = ++num + num++;
+//  preincrement and post increment
+
+// sum => 4
+
+// -------------------------------------******************------------------------------
+*/
+// No.3-----------------------------------------
+console.log("2" + 1 + 1);
+// result => 2111
+console.log(1 + 1 + "2");
+// result=>22
