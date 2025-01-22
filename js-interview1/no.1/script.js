9640965d97320183b31a616f7a4196ee6007f0ef
saurav saha <sauravsaha024@gmail.com>
2025-01-22 16:40:36 +0530

    7 question added

--- js-interview1/no.1/script.js
+++ js-interview1/no.1/script.js
@@ -31,8 +31,95 @@ const sum = ++num + num++;
 
 // -------------------------------------******************------------------------------
 */
+
+/*
 // No.3-----------------------------------------
 console.log("2" + 1 + 1);
 // result => 2111
 console.log(1 + 1 + "2");
 // result=>22
+-----------------------***********-----------------------
+
+
+// No.4--------------------------------
+[a] = [1, 5, 6, 7];
+
+console.log(a);
+
+// result => 1
+//  becouse of destructuring concept************************
+
+// no4----------------------------------------
+
+const yt = "master";
+const it = "teacher";
+
+const r = yt && it;
+
+console.log(r);
+
+
+// if you awant it true and false try this
+
+const yt = "master";
+const it = "teacher";
+
+const r = !!(yt && it);
+
+console.log(r);
+
+
+// no5*****************************
+
+const js = "AB";
+
+if (0 === 1) {
+  js = "CD";
+}
+
+console.log(js);  // result ->AB
+
+
+// const arr = [];
+
+// arr[10] = 10;
+
+// console.log(arr[10].length); // 11
+
+const arr = [];
+
+arr[10] = 10;
+arr[10] = 100;
+
+arr.forEach((d) => console.log("hi"));
+
+// result is =>> hi(2)
+// ---------------------------------------
+// if index is skipted javascript created a empty slot. not initialize indexes in  empty space.. 
+// frach we need indexes
+
+
+
+
+
+// no6---------------------------------------
+
+var show  = 1;
+console.log(show);
+
+function show(){
+    console.log("😅")
+}
+
+// output=>> is 1 1 
+*/
+
+// no7-----------------------------------------------
+
+let arr = [4, 6, 8, 3, 8];
+
+// access arrey in reverse order----------------
+// 1st method----------------------------
+// console.log(arr[arr.length - 1]);
+// 2nd method-----------------------
+console.log(arr.at(-1));
