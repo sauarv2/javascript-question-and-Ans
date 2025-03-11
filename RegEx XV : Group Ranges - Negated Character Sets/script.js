a17df500a0b7006a90b800316e4afacb972f6266
saurav saha <sauravsaha024@gmail.com>
2025-03-11 13:20:36 +0530

    sjbsj

--- RegEx XV : Group Ranges - Negated Character Sets/script.js
+++ RegEx XV : Group Ranges - Negated Character Sets/script.js
@@ -1,3 +1,9 @@
 // /[^a-zA-Z0-9]/g
 
-"alice15@gmail.com".match(/[^a-zA-Z0-9]/g);
+const checkEmail = function (str) {
+  const regex = /[^\w]/gi;
+  const check = str.match(regex);
+  console.log(check);
+};
+
+checkEmail("alice15@gmail.com"); //➞ ["@", "."]
