c45733869ac2d2aaf2d6fcfb366b21f6728f932c
saurav saha <sauravsaha024@gmail.com>
2025-03-17 05:32:02 +0000

    ksnns

---
+++ javascript tricks/script.js
@@ -0,0 +1,40 @@
+const movement = [
+  1,
+  5000,
+  13,
+  -45,
+  1230,
+  -8944,
+  20000,
+  6,
+  56,
+  -2300,
+  80,
+  ,
+  -80000,
+  90,
+  972,
+];
+// **********************************************
+// const check = movement
+//   .flatMap((mov) => mov)
+//   .reduce((mov, curr) => (curr >= 1000 ? mov + 1 : mov), 0);
+
+// console.log(check);
+/*
+const { deposite, withdwal } = movement
+  .flatMap((move) => move)
+  .reduce(
+    (mov, curr) => {
+      curr >= 0 ? (mov.deposite += curr) : (mov.withdwal += curr);
+      return mov;
+    },
+
+    { deposite: 0, withdwal: 0 }
+  );
+console.log(deposite, withdwal);
+
+
+*/
+
+// ************************************************************************
