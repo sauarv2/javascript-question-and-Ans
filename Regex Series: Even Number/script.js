b7916f4f25a870244fe85f6c39c1a3f4254a2483
saurav saha <sauravsaha024@gmail.com>
2025-03-31 02:29:36 +0530

    l

--- Regex Series: Even Number/script.js
+++ Regex Series: Even Number/script.js
@@ -29,3 +29,15 @@ myFunction("132"); //➞  true
 myFunction("29"); //➞  false
 
 myFunction("5578"); //➞ true
+
+// solve by regex*************************
+
+function isEvenNumber(str) {
+  return /^[0-9]*[02468]$/.test(str);
+}
+
+// Test cases
+console.log(isEvenNumber("2341")); // false
+console.log(isEvenNumber("132")); // true
+console.log(isEvenNumber("29")); // false
+console.log(isEvenNumber("5578")); // true
