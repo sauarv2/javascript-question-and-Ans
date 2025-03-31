d5ef1298255b24489741b905026ae4d1c708da23
saurav saha <sauravsaha024@gmail.com>
2025-03-31 14:33:47 +0530

    dk

--- Find the Largest Numbers in a Group of Arrays/script.js
+++ Find the Largest Numbers in a Group of Arrays/script.js
@@ -13,14 +13,52 @@ Notes
 Watch out for negative integers (numbers).
 
 */
+// ************************* 1st method
+/*
+const findLargestNums = (a) => {
+  let result = [];
+  let op;
+  let arr;
+  for (let i = 0; i < a.length; i++) {
+    arr = a[i];
+    op = arr.reduce((prev, acc) => (acc < prev ? prev : acc), arr[0]);
+    result.push(op);
+  }
+  return result;
+  // .reduce((prev, cur) => {
+  //   if (prev < cur) {
+  //     return prev;
+  //   } else {
+  //     return cur;
+  //   }
+  // }, a[0]);
+};
+*/
+/******************************** 2nd method */
 
-const findLargestNums = (a, b, c) => {
-  let arr1 = a[0];
-  console.log(arr1);
+const findLargestNums = (a) => {
+  let result = [];
+  let arr;
+  let Farr;
+  // looping multiplae arrey
+  for (let i = 0; i < a.length; i++) {
+    arr = a[i];
+    Farr = arr[0];
+    // looping one arrey
+    for (let i = 1; i <= a.length; i++) {
+      if (arr[i] > Farr) {
+        Farr = arr[i];
+      }
+    }
+    result.push(Farr); // push data in arrey
+  }
+  return result;
 };
 
-findLargestNums([
-  [4, 2, 7, 1],
-  [20, 70, 40, 90],
-  [1, 2, 0],
-]); // ➞ [7, 90, 2]
+console.log(
+  findLargestNums([
+    [4, 2, 7, 1],
+    [20, 70, 40, 90],
+    [1, 2, 0, 4],
+  ])
+); // ➞ [7, 90, 2]
