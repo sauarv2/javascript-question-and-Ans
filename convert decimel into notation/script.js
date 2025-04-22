eeb39ad86266b685f76812930ff53d5082ffb58c
saurav saha <sauravsaha024@gmail.com>
2025-04-22 22:17:44 +0530

    one more

--- convert decimel into notation/script.js
+++ convert decimel into notation/script.js
@@ -3,3 +3,6 @@ function convertToDecimal(arr) {
 }
 
 convertToDecimal(["1%", "2%", "3%"]); // ➞ [0.01, 0.02, 0.03]
+convertToDecimal(["45%", "32%", "97%", "33%"]); // ➞ [0.45, 0.32, 0.97, 0.33]
+
+convertToDecimal(["33%", "98.1%", "56.44%", "100%"]); // ➞ [0.33, 0.981, 0.5644, 1]
